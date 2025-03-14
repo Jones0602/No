03dbe780d8bc51049e6a4163df6cf120fd0bb0f0
Jones0602 <11221104115@alunos.umc.br>
2025-03-13 22:03:32 -0300

    Update Lista.java

--- src/main/java/com/example/lista/Lista.java
+++ src/main/java/com/example/lista/Lista.java
@@ -10,11 +10,22 @@ public class Lista {
     No novoNo;
 
     public Lista() {
-    }
+        primeiroNo = null;
+    } 
 
     public No getPrimeiroNo() {
         return primeiroNo;
     }
+
+    public boolean estaVazia(){
+        return primeiroNo==null;
+        /*if (primeiroNo==null){
+            return true;
+        }else{
+            return false;
+        }*/
+    }
+
     public void setPrimeiroNo(No primeiroNo) {
         this.primeiroNo = primeiroNo;
     }
@@ -31,25 +42,78 @@ public class Lista {
         this.novoNo = novoNo;
     }
 
-    public void adicionarNo(int novo){
+    /*public void adicionarNo(int novo){
         No novoNo = new No(novo);
         if (primeiroNo == null){
             primeiroNo = novoNo;
         } else {
             ultimoNo = novoNo;
         }
+    }*/
+    public void inserirPrimeiro(int valor){
+        No no1 = new No(Valor);
+        n1.setProximo(primeiroNo);
+        primeiroNo = no1;
+    } 
+
+    public void insereUltimo(int valor) {
+        if (estaVazia()) {
+            inserePrimeiro(valor);
+        } else {
+            No cursor = primeiroNo;
+            while (cursor.getProximo() != null) {
+                cursor = cursor.getProximo();
+         }
+         No no1 = new No(valor);
+            cursor.setProximo(no1);
+        }
     }
 
+
     public void imprimirLista() {
-        No atual = primeiroNo;
+        /*No atual = primeiroNo;
         while (atual != null) {
             System.out.println(atual.getNumeroNo());
             atual.getProximo().setProximo(atual);
             break;
+        }*/
+        if (estaVazia){
+            system.out.println("Lista está vazia");
+        } else {
+            No atual = primerio;
+            while (atual != null) {
+                System.out.println(atual.getNumeroNo());
+                atual = atual.getProximo();
+                break;
+            }
         }
     }
 
-    public void removeNo(No novo){
-
+    public No removePrimeiro() {
+        if (estaVazia()) {
+            System.out.println("ESTA VAZIA, NÃO TEM NADA PRA REMOVER.");
+            return null;
+        } else {
+            No cursor = primeiroNo;
+            primeiroNo = primeiroNo.getProximo();
+            return cursor;
+        }
     }
+    public void removeDepois(No no){
+        if (estaVazia()){
+            System.out.println("ESTA VAZIA");
+        }else{
+            No anterior = null;
+            No cursor = primeiroNo;
+            while(cursor.getProximo() != null){
+                anterior = cursor;
+                cursor = cursor.getProximo();
+                if (cursor.equals(no)){
+                    System.out.println("Nó encontrado com sucesso "+cursor.getNumeroNo());
+                    anterior.setProximo(cursor.getProximo());
+                }
+            }
+        }
+    }
+    
 }
